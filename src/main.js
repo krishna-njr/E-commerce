1adc7b4c6c23370addeb541b58949594821d7d03
keshav <keshav@gmail.com>
2026-06-03 14:38:30 +0530

    server & nodemon

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import express from 'express'; 
+import { config } from 'dotenv'; 
+import app from './app.js';
+
+config(); 
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => {
+  console.log(`server is up and running on http://localhost:${PORT}`)
+}); 
+
+export default app;
