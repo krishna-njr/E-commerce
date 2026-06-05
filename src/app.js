9e7a370aca9ace5e7e01017e65034c0a8c5b69b3
keshav <keshav@gmail.com>
2026-06-05 13:03:08 +0530

    Containerize express application

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express'; 
 import { router as authRoutes } from './modules/auth/route/auth.route.js';
 import userDB from './modules/shared/userDB.js';
+import morgan from 'morgan';
 // import { router as registerRoute } from './modules/auth/register/register.routes.js';
 const app = express(); 
 
 app.use(express.json());
+
+app.use(morgan('dev')); 
 
 app.use('/api/v1/auth', authRoutes);
 
