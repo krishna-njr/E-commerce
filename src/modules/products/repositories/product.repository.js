8ba0689b00d9b62860f33ead532cbf5c779f9501
Your Name <you@example.com>
2026-06-11 16:43:51 +0530

    created product repository

--- src/modules/products/repositories/product.repository.js
+++ src/modules/products/repositories/product.repository.js
@@ -65,3 +65,84 @@ export const getProductsSorted = async (
     throw new AppError(`Database Error in getProductsSorted : ${err.message}`)
   }
 };
+
+
+
+// POST    /products       # Add a product
+// GET     /products       # Get all products
+// GET     /products/:id   # Get products by ID
+// PATCH   /products/:id   # Editing a product
+// DELETE  /products/:id   # Deleting a product
+
+
+
+export const addProduct = async ({name, description, price}) => {
+  try {
+    return await prisma.product.create({
+      data: {
+        name: name, 
+        description: description, 
+        price: price, 
+
+        inventory: {
+          create: {}
+        }, 
+
+        // cartItems: {
+        //   create: [], 
+        // },
+
+        // orderItems: {
+        //   create: [], 
+        // }, 
+
+      }
+    });
+  } catch (err) {
+    console.error(`Database Error in addProduct : `, err.message);
+    throw new AppError(`Database Error in addProduct : ${err.message}`)
+  }
+};
+
+export const getProductById = async (productId) => {
+  try {
+    return await prisma.product.findUnique({
+      where: {
+        id: productId
+      },  
+    });
+  } catch (err) {
+    console.error("Database Error in getProductById:", err.message);
+    throw new AppError(`Database Error in getProductById : ${err.message}`)
+  }
+};
+
+
+export const deleteProductById = async (productId) => {
+  try {
+    return await prisma.product.delete({
+      where: {
+        id: productId
+      },  
+    });
+  } catch (err) {
+    console.error("Database Error in deleteProductById:", err.message);
+    throw new AppError(`Database Error in deleteProductById : ${err.message}`)
+  }
+};
+
+export const editProductById = async (productId, productDetails) => {
+  try {
+    return await prisma.product.update({
+      where: {
+        id: productId
+      },  
+      data: {
+        ...productDetails
+      }
+    });
+  } catch (err) {
+    console.error("Database Error in deleteProductById:", err.message);
+    throw new AppError(`Database Error in deleteProductById : ${err.message}`)
+  }
+};
