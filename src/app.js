77955677cc56948b20d75e909e91bd0d0fa26341
Your Name <you@example.com>
2026-06-12 12:22:12 +0530

    orders controller

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-import express from "express";
+import express, { application } from "express";
 import morgan from "morgan";
 import { router as authRoutes } from "./modules/auth/route/auth.route.js";
 import { router as userRoutes } from "./modules/user/route/user.route.js";
 import { router as publicProductRoutes } from "./modules/products/route/product.public.route.js";
 import { router as sellerProductRoutes } from "./modules/products/route/product.seller.route.js";
-
+import { router as orderRoutes } from "./modules/orders/route/order.route.js";
 const app = express();
 
 app.use(express.json());
 
 app.use(morgan("dev"));
 
-app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/users", userRoutes);
 
-app.use("/api/v1/product", publicProductRoutes, sellerProductRoutes);
+app.use("/api/v1/products", publicProductRoutes, sellerProductRoutes);
+
+app.use('/api/v1/orders', orderRoutes);
 
 // app.all('/test', (req, res) => {
 //   console.log(req.body);
--- src/modules/orders/controller/order.controller.js
+++ src/modules/orders/controller/order.controller.js
@@ -1,11 +1,82 @@
-export const createOrderController = async (req, res) => {};
+import { asyncWrapper } from "../../../../utils/asyncWrapper.js";
+import * as orderServices from "../service/order.service.js";
 
-export const getOrdersController = async (req, res) => {};
+export const createOrderController = asyncWrapper(async (req, res) => {
 
-export const getOrderByIdController = async (req, res) => {};
 
-export const updateOrderStatusController = async (req, res) => {};
+  // const userId = req.user.id; 
+  const userId = req.body.id;
+  const items = req.body.items;
 
-export const updatePaymentStatusController = async (req, res) => {};
+  const createdOrder = await orderServices.createOrderService(userId, items);
 
-export const cancelOrderController = async (req, res) => {};
+  res.status(201).json({
+    status: true,
+    message: 'Order Created',
+    date: createdOrder,
+  })
+});
+
+export const getOrdersController = asyncWrapper(async (req, res) => {
+
+  const order = await orderServices.getOrdersService();
+
+  res.status(200).json({
+    status: true,
+    message: 'Successfully Retrieved',
+    date: order,
+  });
+
+});
+
+export const getOrderByIdController = asyncWrapper(async (req, res) => {
+
+  const productId = req.params.id;
+
+  const order = await orderServices.getOrderByIdService(productId);
+
+  res.status(200).json({
+    status: true,
+    message: 'Successfully Retrieved',
+    date: order,
+  })
+});
+
+export const updateOrderStatusController = asyncWrapper(async (req, res) => {
+  const productId = req.params.id;
+  const status = req.query.status;
+
+  const updatedOrder = await orderServices.updateOrderStatusService(productId, status);
+
+  res.status(201).json({
+    status: true,
+    message: 'Order Updated',
+    date: updatedOrder,
+  })
+});
+
+export const updatePaymentStatusController = asyncWrapper(async (req, res) => {
+  const productId = req.params.id;
+  const paymentStatus = req.query.paymentStatus;
+
+  const updatedOrder = await orderServices.updatePaymentStatusService(productId, paymentStatus);
+
+  res.status(201).json({
+    status: true,
+    message: 'Status Updated',
+    date: updatedOrder,
+  })
+});
+
+export const cancelOrderController = asyncWrapper(async (req, res) => {
+
+  const productId = req.query.id;
+
+  const deletedOrder = await orderServices.cancelOrderService(productId);
+
+  res.status(201).json({
+    status: true,
+    message: 'Order Deleted',
+    date: deletedOrder,
+  })
+});
