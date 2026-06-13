7e5a2fe0ab073d2ed9dff373ee4c74c428e4f572
krishna-njr <keshavverma020@gmail.com>
2026-06-13 15:44:55 +0530

    fix successResponse product controller

--- src/modules/products/controller/product.public.controller.js
+++ src/modules/products/controller/product.public.controller.js
@@ -1,42 +1,33 @@
 import { asyncWrapper } from "../../../../utils/asyncWrapper.js";
 import * as productService from "../service/product.service.js";
+import successResponse from "../../../../utils/responseHelper.js";
 
 export const getAllProducts = asyncWrapper(async (req, res) => {
-
   const products = await productService.getAllProductsService();
-  res.status(200).json({ status: true, data: products });
-
+  return successresponse(res, products, "Products retrieved successfully", 200);
 });
 
 export const searchProducts = asyncWrapper(async (req, res) => {
-
   const productName = req.query.name; // e.g., /products/search?name=phone
   const products = await productService.searchProductService(productName);
-  res.status(200).json({ status: true, data: products });
-
+  return successResponse(res, products, "Products retrieved successfully", 200);
 });
 
 export const getPaginatedProducts = asyncWrapper(async (req, res) => {
-
   const { page, limit } = req.query; // e.g., /products/browse?page=2&limit=10
   const products = await productService.getPaginatedProductsService(
     page,
     limit,
   );
-  res
-    .status(200)
-    .json({ status: true, page: Number(page) || 1, data: products });
-
+  return successResponse(res, products, "Products retrieved successfully", 200);
 });
 
 export const getSortedProducts = asyncWrapper(async (req, res) => {
-
   const { type, sortBy, order } = req.query;
   const products = await productService.getSortedProductsService(
     type,
     sortBy,
     order,
   );
-  res.status(200).json({ status: true, data: products });
-
+  return successResponse(res, products, "Products retrieved successfully", 200);
 });
