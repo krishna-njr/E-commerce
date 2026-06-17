dc113d2b12af927cf727d07bc38d7503bb445c85
krishna-njr <keshavverma020@gmail.com>
2026-06-17 13:07:46 +0530

    database check

--- clients/prisma.client.js
+++ clients/prisma.client.js
@@ -2,4 +2,15 @@ import { PrismaClient } from "../src/generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
+async function checkConnection() {
+  try {
+    await prisma.$connect();
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection error:", error.message);
+  }
+}
+
+checkConnection();
+
 export { prisma };
